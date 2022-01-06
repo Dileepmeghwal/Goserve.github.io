6455c81a5f3237de88fe33c5a351f34c80fb4448
Dileep Meghwal <dileepmeghwalrock275@gmail.com>
2022-01-06 11:46:46 +0530

    header changed

--- assets/js/jquery.js
+++ assets/js/jquery.js
@@ -1,6 +1,6 @@
-$(".nav__item").on('click','li',function(){
-    // remove classname 'active' from all li who already has classname 'active'
-    $(".nav__item .nav__link.active").removeClass("active"); 
+$("#navMenu").on('click', 'nav__item, function () {
+    $("#navMenu nav__link.active").removeClass("active");
     // adding classname 'active' to current click li 
-    $(this).addClass("active"); 
-}); 
+    $(this).addClass("active");
+});
+
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,25 +1,66 @@
 
 
 /*==================== SHOW MENU ====================*/
-const navMenu = document.getElementById('nav-menu'),
-      navToggle = document.getElementById('nav-toggle'),
-      navClose = document.getElementById('nav-close')
+(()=>{
+    const openNavMenu = document.querySelector(".open-nav-menu"),
+    closeNavMenu = document.querySelector(".close-nav-menu"),
+    navMenu = document.querySelector(".nav-menu");
+    menuOverlay = document.querySelector(".menu-overlay"),
+    mediaSize = 991;
 
-/*===== MENU SHOW =====*/
-/* Validate if constant exists */
-if(navToggle){
-    navToggle.addEventListener('click', () =>{
-        navMenu.classList.add('show-menu')
-    })
-}
 
-/*===== MENU HIDDEN =====*/
-/* Validate if constant exists */
-if(navClose){
-    navClose.addEventListener('click', () =>{
-        navMenu.classList.remove('show-menu')
-    })
-}
+    openNavMenu.addEventListener("click", toggleNav);
+    closeNavMenu.addEventListener("click", toggleNav);
+    menuOverlay.addEventListener("click", toggleNav);
+
+    function toggleNav(){
+        navMenu.classList.toggle("open");
+        menuOverlay.classList.toggle("active")
+        document.body.classList.toggle("hidden-scrolling");
+    }
+
+    navMenu.addEventListener("click", (event) =>{
+        if(event.target.hasAttribute("data-toggle") &&
+        window.innerWidth <= mediaSize){
+            const menuItemHasChildern = event.target.parentElement;
+
+            if(menuItemHasChildern.classList.contains("active")){
+                collapseSubMenu();
+            }
+            else {
+
+
+                if (navMenu.querySelector(".menu-item-has-childern.active")) {
+                    collapseSubMenu();
+                }
+                menuItemHasChildern.classList.add("active");
+                const subMenu = menuItemHasChildern.querySelector(".sub-menu");
+                subMenu.style.maxHeight = subMenu.scrollHeight + "px";
+            }
+        }
+        
+    });
+
+    function collapseSubMenu(){
+        navMenu.querySelector(".menu-item-has-childern.active .sub-menu")
+        .removeAttribute("style");
+        navMenu.querySelector(".menu-item-has-childern.active")
+        .classList.remove("active")
+    }
+    function resizeFix(){
+        if (navMenu.classList.contains("open")){
+            toggleNav();
+        }
+        
+    }
+
+
+    window.addEventListener("resize", function(){
+        if(this.innerWidth > mediaSize){
+            resizeFix();
+        }
+    });
+})(); 
 
 /*==================== REMOVE MENU MOBILE ====================*/
 const navLink = document.querySelectorAll('.nav__link')
