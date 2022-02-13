76cedf9882cee9a01b10db0c4242fc51139adcb6
Dileep Meghwal <dileepmeghwalrock275@gmail.com>
2022-02-13 22:18:12 +0530

    main

--- assets/js/swiper.js
+++ assets/js/swiper.js
@@ -1,17 +1,10 @@
 var swiper = new Swiper(".mySwiper", {
-    effect: "coverflow",
-    grabCursor: true,
-    centeredSlides: true,
-    slidesPerView: "auto",
-    coverflowEffect: {
-      rotate: 0,
-      stretch: 0,
-      depth: 500,
-      modifier: 1,
-      slideShadows: true,
-    },
-    loop: true,
-    pagination: {
-      el: ".swiper-pagination",
-    },
-  });
+  pagination: {
+    el: ".swiper-pagination",
+    type: "progressbar",
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
