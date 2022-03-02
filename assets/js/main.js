dd0ac5059713297688b4d488e7c880fa3fd8f148
Dileep Meghwal <dileepmeghwalrock275@gmail.com>
2022-03-02 16:52:33 +0530

    main

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,73 +1,39 @@
+// search-box open close js code
+let navbar = document.querySelector(".navbar");
+let searchBox = document.querySelector(".search-box .bx-search");
+// let searchBoxCancel = document.querySelector(".search-box .bx-x");
 
+searchBox.addEventListener("click", ()=>{
+  navbar.classList.toggle("showInput");
+  if(navbar.classList.contains("showInput")){
+    searchBox.classList.replace("bx-search" ,"bx-x");
+  }else {
+    searchBox.classList.replace("bx-x" ,"bx-search");
+  }
+});
 
-/*==================== SHOW MENU ====================*/
-(()=>{
-    const openNavMenu = document.querySelector(".open-nav-menu"),
-    closeNavMenu = document.querySelector(".close-nav-menu"),
-    navMenu = document.querySelector(".nav-menu");
-    menuOverlay = document.querySelector(".menu-overlay"),
-    mediaSize = 991;
-
-
-    openNavMenu.addEventListener("click", toggleNav);
-    closeNavMenu.addEventListener("click", toggleNav);
-    menuOverlay.addEventListener("click", toggleNav);
-
-    function toggleNav(){
-        navMenu.classList.toggle("open");
-        menuOverlay.classList.toggle("active")
-        document.body.classList.toggle("hidden-scrolling");
-    }
-
-    navMenu.addEventListener("click", (event) =>{
-        if(event.target.hasAttribute("data-toggle") &&
-        window.innerWidth <= mediaSize){
-            const menuItemHasChildern = event.target.parentElement;
-
-            if(menuItemHasChildern.classList.contains("active")){
-                collapseSubMenu();
-            }
-            else {
-
-
-                if (navMenu.querySelector(".menu-item-has-childern.active")) {
-                    collapseSubMenu();
-                }
-                menuItemHasChildern.classList.add("active");
-                const subMenu = menuItemHasChildern.querySelector(".sub-menu");
-                subMenu.style.maxHeight = subMenu.scrollHeight + "px";
-            }
-        }
-        
-    });
-
-    function collapseSubMenu(){
-        navMenu.querySelector(".menu-item-has-childern.active .sub-menu")
-        .removeAttribute("style");
-        navMenu.querySelector(".menu-item-has-childern.active")
-        .classList.remove("active")
-    }
-    function resizeFix(){
-        if (navMenu.classList.contains("open")){
-            toggleNav();
-        }
-        
-    }
-
-
-    window.addEventListener("resize", function(){
-        if(this.innerWidth > mediaSize){
-            resizeFix();
-        }
-    });
-})(); 
-
-/*==================== REMOVE MENU MOBILE ====================*/
-const navLink = document.querySelectorAll('.nav__link')
-
-function linkAction(){
-    const navMenu = document.getElementById('nav-menu')
-    // When we click on each nav__link, we remove the show-menu class
-    navMenu.classList.remove('show-menu')
+// sidebar open close js code
+let navLinks = document.querySelector(".nav-links");
+let menuOpenBtn = document.querySelector(".navbar .bx-menu");
+let menuCloseBtn = document.querySelector(".nav-links .bx-x");
+menuOpenBtn.onclick = function() {
+navLinks.style.left = "0";
 }
-navLink.forEach(n => n.addEventListener('click', linkAction))
+menuCloseBtn.onclick = function() {
+navLinks.style.left = "-100%";
+}
+
+
+// sidebar submenu open close js code
+let htmlcssArrow = document.querySelector(".htmlcss-arrow");
+htmlcssArrow.onclick = function() {
+ navLinks.classList.toggle("show1");
+}
+let moreArrow = document.querySelector(".more-arrow");
+moreArrow.onclick = function() {
+ navLinks.classList.toggle("show2");
+}
+let jsArrow = document.querySelector(".js-arrow");
+jsArrow.onclick = function() {
+ navLinks.classList.toggle("show3");
+}
