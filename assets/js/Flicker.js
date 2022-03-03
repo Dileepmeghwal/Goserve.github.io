74bfb08333535e70dcc70a23d5753d8a78ca24c5
Dileep Meghwal <dileepmeghwalrock275@gmail.com>
2022-03-04 00:13:43 +0530

    banner

--- assets/js/Flicker.js
+++ assets/js/Flicker.js
@@ -3,7 +3,7 @@ $('.slider').slick({
     dots: true,
     vertical: false,
     draggable: true,
-    autoplay: true,
+    autoplay: false,
     autoplaySpeed: 3000,
     cssEase: 'ease',
     mobileFirst: true,
