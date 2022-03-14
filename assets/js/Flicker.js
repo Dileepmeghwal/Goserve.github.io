1917d3d827e8912a9d46dcc683d59f2e3c3bc4ff
Dileep Meghwal <dileepmeghwalrock275@gmail.com>
2022-03-14 14:15:44 +0530

    footer updated

--- assets/js/Flicker.js
+++ assets/js/Flicker.js
@@ -1,12 +1,13 @@
 
-$('.slider').slick({
-    dots: true,
-    vertical: false,
-    draggable: true,
-    autoplay: false,
-    autoplaySpeed: 3000,
-    cssEase: 'ease',
-    mobileFirst: true,
-    swipe: true,
-    touchMove: true,
-});
+// $('.slider').slick({
+//     dots: true,
+//     vertical: false,
+//     draggable: true,
+//     autoplay: false,
+//     autoplaySpeed: 3000,
+//     cssEase: 'ease',
+//     mobileFirst: true,
+//     swipe: true,
+//     touchMove: true,
+// });
+
--- assets/js/bannerSlider.js
+++ assets/js/bannerSlider.js
@@ -1,20 +1,22 @@
-$(document).ready(function(){
+$(document).ready(function () {
     $('.owl-carousel').owlCarousel({
-        loop:true,
-        margin:0,
-        nav:true,
+        loop: true,
+        margin: 0,
+        nav: true,
         autoPlay: 1000,
-        slideSpeed : 600,
+        autoplayTimeout: 2000,  
+        autoplayHoverPause: true,
+        slideSpeed: 600,
         animateOut: 'fadeOut',
-        responsive:{
-            0:{
-                items:1
+        responsive: {
+            0: {
+                items: 1
             },
-            600:{
-                items:1
+            600: {
+                items: 1
             },
-            1000:{
-                items:1
+            1000: {
+                items: 1
             }
         }
     })
@@ -23,8 +25,8 @@ $(document).ready(function(){
 
 // vars
 'use strict'
-var	testim = document.getElementById("testim"),
-		testimDots = Array.prototype.slice.call(document.getElementById("testim-dots").children),
+var testim = document.getElementById("testim"),
+    testimDots = Array.prototype.slice.call(document.getElementById("testim-dots").children),
     testimContent = Array.prototype.slice.call(document.getElementById("testim-content").children),
     testimLeftArrow = document.getElementById("left-arrow"),
     testimRightArrow = document.getElementById("right-arrow"),
@@ -32,101 +34,101 @@ var	testim = document.getElementById("testim"),
     currentSlide = 0,
     currentActive = 0,
     testimTimer,
-		touchStartPos,
-		touchEndPos,
-		touchPosDiff,
-		ignoreTouch = 30;
+    touchStartPos,
+    touchEndPos,
+    touchPosDiff,
+    ignoreTouch = 30;
 ;
 
-window.onload = function() {
+// window.onload = function () {
 
-    // Testim Script
-    function playSlide(slide) {
-        for (var k = 0; k < testimDots.length; k++) {
-            testimContent[k].classList.remove("active");
-            testimContent[k].classList.remove("inactive");
-            testimDots[k].classList.remove("active");
-        }
+//     // Testim Script
+//     function playSlide(slide) {
+//         for (var k = 0; k < testimDots.length; k++) {
+//             testimContent[k].classList.remove("active");
+//             testimContent[k].classList.remove("inactive");
+//             testimDots[k].classList.remove("active");
+//         }
 
-        if (slide < 0) {
-            slide = currentSlide = testimContent.length-1;
-        }
+//         if (slide < 0) {
+//             slide = currentSlide = testimContent.length - 1;
+//         }
 
-        if (slide > testimContent.length - 1) {
-            slide = currentSlide = 0;
-        }
+//         if (slide > testimContent.length - 1) {
+//             slide = currentSlide = 0;
+//         }
 
-        if (currentActive != currentSlide) {
-            testimContent[currentActive].classList.add("inactive");            
-        }
-        testimContent[slide].classList.add("active");
-        testimDots[slide].classList.add("active");
+//         if (currentActive != currentSlide) {
+//             testimContent[currentActive].classList.add("inactive");
+//         }
+//         testimContent[slide].classList.add("active");
+//         testimDots[slide].classList.add("active");
 
-        currentActive = currentSlide;
-    
-        clearTimeout(testimTimer);
-        testimTimer = setTimeout(function() {
-            playSlide(currentSlide += 1);
-        }, testimSpeed)
-    }
+//         currentActive = currentSlide;
 
-    testimLeftArrow.addEventListener("click", function() {
-        playSlide(currentSlide -= 1);
-    })
+//         clearTimeout(testimTimer);
+//         testimTimer = setTimeout(function () {
+//             playSlide(currentSlide += 1);
+//         }, testimSpeed)
+//     }
 
-    testimRightArrow.addEventListener("click", function() {
-        playSlide(currentSlide += 1);
-    })    
+//     testimLeftArrow.addEventListener("click", function () {
+//         playSlide(currentSlide -= 1);
+//     })
 
-    for (var l = 0; l < testimDots.length; l++) {
-        testimDots[l].addEventListener("click", function() {
-            playSlide(currentSlide = testimDots.indexOf(this));
-        })
-    }
+//     testimRightArrow.addEventListener("click", function () {
+//         playSlide(currentSlide += 1);
+//     })
 
-    playSlide(currentSlide);
+//     for (var l = 0; l < testimDots.length; l++) {
+//         testimDots[l].addEventListener("click", function () {
+//             playSlide(currentSlide = testimDots.indexOf(this));
+//         })
+//     }
 
-    // keyboard shortcuts
-    document.addEventListener("keyup", function(e) {
-        switch (e.keyCode) {
-            case 37:
-                testimLeftArrow.click();
-                break;
-                
-            case 39:
-                testimRightArrow.click();
-                break;
+//     playSlide(currentSlide);
 
-            case 39:
-                testimRightArrow.click();
-                break;
+//     // keyboard shortcuts
+//     document.addEventListener("keyup", function (e) {
+//         switch (e.keyCode) {
+//             case 37:
+//                 testimLeftArrow.click();
+//                 break;
 
-            default:
-                break;
-        }
-    })
-		
-		testim.addEventListener("touchstart", function(e) {
-				touchStartPos = e.changedTouches[0].clientX;
-		})
-	
-		testim.addEventListener("touchend", function(e) {
-				touchEndPos = e.changedTouches[0].clientX;
-			
-				touchPosDiff = touchStartPos - touchEndPos;
-			
-				console.log(touchPosDiff);
-				console.log(touchStartPos);	
-				console.log(touchEndPos);	
+//             case 39:
+//                 testimRightArrow.click();
+//                 break;
 
-			
-				if (touchPosDiff > 0 + ignoreTouch) {
-						testimLeftArrow.click();
-				} else if (touchPosDiff < 0 - ignoreTouch) {
-						testimRightArrow.click();
-				} else {
-					return;
-				}
-			
-		})
-}
+//             case 39:
+//                 testimRightArrow.click();
+//                 break;
+
+//             default:
+//                 break;
+//         }
+//     })
+
+//     testim.addEventListener("touchstart", function (e) {
+//         touchStartPos = e.changedTouches[0].clientX;
+//     })
+
+//     testim.addEventListener("touchend", function (e) {
+//         touchEndPos = e.changedTouches[0].clientX;
+
+//         touchPosDiff = touchStartPos - touchEndPos;
+
+//         console.log(touchPosDiff);
+//         console.log(touchStartPos);
+//         console.log(touchEndPos);
+
+
+//         if (touchPosDiff > 0 + ignoreTouch) {
+//             testimLeftArrow.click();
+//         } else if (touchPosDiff < 0 - ignoreTouch) {
+//             testimRightArrow.click();
+//         } else {
+//             return;
+//         }
+
+//     })
+// }
