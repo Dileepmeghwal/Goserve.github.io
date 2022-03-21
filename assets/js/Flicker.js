cd9f3590430cdfb8be049c4b31015129402b7e8a
Dileep Meghwal <dileepmeghwalrock275@gmail.com>
2022-03-21 14:09:06 +0530

    fade-animation

--- assets/js/Flicker.js
+++ assets/js/Flicker.js
@@ -3,9 +3,9 @@ AOS.init({
      // values from 0 to 3000, with step 50ms
     duration: 2000, // values from 0 to 3000, with step 50ms
     easing: 'ease', // default easing for AOS animations
-    once: false, // whether animation should happen only once - while scrolling down
+    // once: false, // whether animation should happen only once - while scrolling down
     mirror: false, // whether elements should animate out while scrolling past them
-    anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
+    anchorPlacement: 'top', // defines which position of the element regarding to window should trigger the animation
     debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
     throttleDelay: 99,
 
